3fad692c09aaa3ef9ac7a463ff5a547f5aace7ae
Eduardo Callado <eduardo.callado@ideais.com.br>
2013-09-17 18:19:19 -0300

    Adding a debug library, DCIntrospect

--- Demo/PhotoBrowserDemo/AppDelegate.m
+++ Demo/PhotoBrowserDemo/AppDelegate.m
@@ -8,6 +8,7 @@
 
 #import "AppDelegate.h"
 #import "Menu.h"
+#import "DCIntrospect.h"
 
 @implementation AppDelegate
 
@@ -15,12 +16,17 @@
 @synthesize viewController = _viewController;
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
-{
+{    
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     Menu *menu = [[Menu alloc] initWithStyle:UITableViewStyleGrouped];
     self.viewController = (UIViewController *)[[UINavigationController alloc] initWithRootViewController:menu];
     self.window.rootViewController = self.viewController;
     [self.window makeKeyAndVisible];
+    
+#if TARGET_IPHONE_SIMULATOR
+    [[DCIntrospect sharedIntrospector] start];
+#endif
+    
     return YES;
 }
 
