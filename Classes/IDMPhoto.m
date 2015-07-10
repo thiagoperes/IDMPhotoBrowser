c806979f6622e5eb2e473e1fd062c39539f7a7da
Séraphin Hochart <craftpaper@gmail.com>
2015-07-10 16:10:18 -0400

    Accept more image content-type

--- Classes/IDMPhoto.m
+++ Classes/IDMPhoto.m
@@ -142,8 +142,8 @@ caption = _caption;
             
             AFHTTPRequestOperation *op = [[AFHTTPRequestOperation alloc] initWithRequest:request];
             op.responseSerializer = [AFImageResponseSerializer serializer];
-	    op.responseSerializer.acceptableContentTypes = [NSSet setWithObject:@"image/jpg"];
-	    op.responseSerializer.acceptableContentTypes = [NSSet setWithObject:@"image/jpeg"];
+            op.responseSerializer.acceptableContentTypes = [NSSet setWithObjects:@"image/jpg", @"image/jpeg", nil];
+
             [op setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
                 UIImage *image = responseObject;
                 self.underlyingImage = image;
