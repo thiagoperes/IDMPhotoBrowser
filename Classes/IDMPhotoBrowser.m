d00c1fd9a33cd28bc217d4507555fa35593823ac
Kaan Dedeoglu <kaandedeoglu@me.com>
2015-08-18 09:23:56 +0300

    Use the correct bundle, fixes issues when used as a framework

--- Classes/IDMPhotoBrowser.m
+++ Classes/IDMPhotoBrowser.m
@@ -14,7 +14,7 @@
 
 #ifndef IDMPhotoBrowserLocalizedStrings
 #define IDMPhotoBrowserLocalizedStrings(key) \
-NSLocalizedStringFromTableInBundle((key), nil, [NSBundle bundleWithPath:[[NSBundle mainBundle] pathForResource:@"IDMPBLocalizations" ofType:@"bundle"]], nil)
+NSLocalizedStringFromTableInBundle((key), nil, [NSBundle bundleWithPath:[[NSBundle bundleForClass: self.class] pathForResource:@"IDMPBLocalizations" ofType:@"bundle"]], nil)
 #endif
 
 // Private
