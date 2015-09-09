48c4e2804b9e16171463d5b6151a5ec7782f2d50
Michal Stawarz <mikestawarz@gmail.com>
2015-09-09 12:47:42 +0200

    New photoBrowser delegate method that informs user which activityType has been selected in UIActivityViewController

--- Classes/IDMPhotoBrowser.h
+++ Classes/IDMPhotoBrowser.h
@@ -22,6 +22,7 @@
 - (void)photoBrowser:(IDMPhotoBrowser *)photoBrowser willDismissAtPageIndex:(NSUInteger)index;
 - (void)photoBrowser:(IDMPhotoBrowser *)photoBrowser didDismissActionSheetWithButtonIndex:(NSUInteger)buttonIndex photoIndex:(NSUInteger)photoIndex;
 - (IDMCaptionView *)photoBrowser:(IDMPhotoBrowser *)photoBrowser captionViewForPhotoAtIndex:(NSUInteger)index;
+- (void)photoBrowser:(IDMPhotoBrowser *)photoBrowser activityViewControllerActivitySelected:(NSString *)activityType;
 @end
 
 // IDMPhotoBrowser
