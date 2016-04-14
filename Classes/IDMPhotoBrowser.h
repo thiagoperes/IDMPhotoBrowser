0daca105649761ff05cb68ade80a4dc7ea5a86b1
Philippe Auriach <p.auriach@gmail.com>
2016-04-14 19:26:20 +0200

    added property autoHideInterface

--- Classes/IDMPhotoBrowser.h
+++ Classes/IDMPhotoBrowser.h
@@ -53,6 +53,9 @@
 @property (nonatomic) BOOL usePopAnimation;
 @property (nonatomic) BOOL disableVerticalSwipe;
 
+/* default value: true. set to false to tell the photo viewer not to hide the interface when scrolling */
+@property (nonatomic) BOOL autoHideInterface;
+
 // defines zooming of the background (default 1.0)
 @property (nonatomic) float backgroundScaleFactor;
 
