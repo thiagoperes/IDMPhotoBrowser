eaced5b527297d7b0616fa557c8bc44888d96de0
Eduardo Callado <eduardo_tasker@hotmail.com>
2017-05-24 21:02:45 -0300

    Fix SDWebImageManager method call

--- Classes/IDMPhoto.m
+++ Classes/IDMPhoto.m
@@ -136,19 +136,20 @@ caption = _caption;
             [self performSelectorInBackground:@selector(loadImageFromFileAsync) withObject:nil];
         } else if (_photoURL) {
             // Load async from web (using SDWebImageManager)
-            [[SDWebImageManager sharedManager] downloadImageWithURL:_photoURL options:SDWebImageRetryFailed|SDWebImageHandleCookies progress:^(NSInteger receivedSize, NSInteger expectedSize) {
-                CGFloat progress = ((CGFloat)receivedSize)/((CGFloat)expectedSize);
-                
-                if (self.progressUpdateBlock) {
-                    self.progressUpdateBlock(progress);
-                }
-            } completed:^(UIImage *image, NSError *error, SDImageCacheType cacheType, BOOL finished, NSURL *imageURL) {
-                if (image) {
-                    self.underlyingImage = image;
-                }
-                
-                [self performSelectorOnMainThread:@selector(imageLoadingComplete) withObject:nil waitUntilDone:NO];
-            }];
+			
+			[[SDWebImageManager sharedManager] loadImageWithURL:_photoURL options:SDWebImageRetryFailed progress:^(NSInteger receivedSize, NSInteger expectedSize, NSURL * _Nullable targetURL) {
+				CGFloat progress = ((CGFloat)receivedSize)/((CGFloat)expectedSize);
+				
+				if (self.progressUpdateBlock) {
+					self.progressUpdateBlock(progress);
+				}
+			} completed:^(UIImage * _Nullable image, NSData * _Nullable data, NSError * _Nullable error, SDImageCacheType cacheType, BOOL finished, NSURL * _Nullable imageURL) {
+				if (image) {
+					self.underlyingImage = image;
+				}
+				
+				[self performSelectorOnMainThread:@selector(imageLoadingComplete) withObject:nil waitUntilDone:NO];
+			}];
         } else {
             // Failed - no source
             self.underlyingImage = nil;
